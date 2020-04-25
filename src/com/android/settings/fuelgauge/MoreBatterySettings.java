29e38d17d71f46c16799289a849ebdab0917a326
TheStrix <parthbhatia98@gmail.com>
2020-04-25 14:26:42 +0200

    Settings: Features: Add statusbar battery options [2/2]
    Allows the user to tune:
    - battery styles
    - battery percentage
    
    rcstar6696:
    - Adapted to our battery styles
    
    abc: adapt to Settings
    stx: add pt-br translation
    Change-Id: Icf9d072442d09b3a5dada4c21404eff5539d5c12
    
    -----
    
    Settings: Battery options cleanup [2/2]
    
    Change-Id: If4cc623252580500e7406ca093ea2a4a0f3e7084
    
    -----
    
    Settings: Remove aosp battery percentage option
    
    Change-Id: Ie36b594cbcde7777470cd698ab40cd2aaf3f004c
    Co-authored-by: ezio84 <brabus84@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/settings/fuelgauge/MoreBatterySettings.java
+++ src/com/android/settings/fuelgauge/MoreBatterySettings.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,43 +13,78 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.settings.fuelgauge;
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
 
-import com.android.settings.dashboard.DashboardFragment;
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.halcyon.preference.SystemSettingListPreference;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MoreBatterySettings extends DashboardFragment {
+public class MoreBatterySettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
-    public static final String TAG = "Customizer";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_battery_percent";
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
 
-    private Context mContext;
+    private SystemSettingListPreference mStatusBarBatteryShowPercent;
+    private PreferenceCategory mStatusBarBatteryCategory;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mContext = getActivity();
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.halcyon_more_battery);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mStatusBarBatteryShowPercent = findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        SystemSettingListPreference statusBarBattery =
+                findPreference(STATUS_BAR_BATTERY_STYLE);
+        statusBarBattery.setOnPreferenceChangeListener(this);
+        int value = Integer.parseInt(statusBarBattery.getValue());
+        enableStatusBarBatteryDependents(value);
     }
 
     @Override
-    protected String getLogTag() {
-        return TAG;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int value = Integer.parseInt((String) newValue);
+        String key = preference.getKey();
+        switch (key) {
+            case STATUS_BAR_BATTERY_STYLE:
+                enableStatusBarBatteryDependents(value);
+                break;
+        }
+        return true;
     }
 
-    @Override
-    protected int getPreferenceScreenResId() {
-        return R.xml.halcyon_more_battery;
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
+
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.HALCYON;
+        return MetricsProto.MetricsEvent.HALCYON;
     }
 }
